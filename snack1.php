87b0da6045663e34ab1761bdabfa6b5314fe1741
Gabridg <gabri99.del@gmail.com>
2022-08-30 15:52:26 +0200

    setted array of matches

---
+++ snack1.php
@@ -0,0 +1,28 @@
+<?php 
+$nba_matches = [
+    [
+        "home" => "Phoenix",
+        "visiting" => "Chicago",
+        "pointsT1" => 42,
+        "pointsT2" => 90,
+    ],
+    [
+        "home" => "Pelicans",
+        "visiting" => "Lakers",
+        "pointsT1" => 114,
+        "pointsT2" => 111,
+    ],
+    [
+        "home" => "Celtics",
+        "visiting" => "NYKnicks",
+        "pointsT1" => 117,
+        "pointsT2" => 84,
+    ],
+    [
+        "home" => "Cavaliers",
+        "visiting" => "Milwaukee",
+        "pointsT1" => 115,
+        "pointsT2" => 103,
+    ]
+    ];
+?>
