7fbb1495e07b5d6d605db088f1f9346bae8c13fb
Gabridg <gabri99.del@gmail.com>
2022-08-30 16:15:05 +0200

    snack1 complete

--- snack1.php
+++ snack1.php
@@ -25,4 +25,36 @@ $nba_matches = [
         "pointsT2" => 103,
     ]
     ];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <ul>
+            <?php for($i = 0; $i < count($nba_matches); $i++) : ?>
+            <li>
+                <span class="team">
+                    <?= $nba_matches[$i]['home'] ?>
+                    /
+                    <?= $nba_matches[$i]['visiting'] ?>
+                    ||
+                </span>    
+                <span class="points">
+                    <?= $nba_matches[$i]['pointsT1'] ?>
+                    -
+                    <?= $nba_matches[$i]['pointsT2'] ?>
+                </span>
+            </li>
+            <?php endfor; ?>
+        </ul>
+    </div>
+</body>
+</html>
