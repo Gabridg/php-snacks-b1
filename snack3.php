01e38d6db8ae5bbcf096c0e77647371e6f6add90
Gabridg <gabri99.del@gmail.com>
2022-08-31 10:11:27 +0200

    task3 start

---
+++ snack3.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+*/
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>post</h1>
+    <?php for($i = 0; $i < count($posts); $i++) : ?>
+        <?php $posts[$i] == $post ?>
+        <?php for($j = 0; $j < count($post); $j++) : ?>
+            <p><?= $post[$j]['title'] ?></p>
+            <p><?= $post[$j]['author'] ?></p>
+            <p><?= $post[$j]['text'] ?></p>
+        <?php endfor; ?>
+    <?php endfor; ?>
+</body>
+</html>
